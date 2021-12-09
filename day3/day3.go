6f44a6726ee4ca0a69af7f3ac2b6812859ffe594
Joakim Hansson <joakimhew@gmail.com>
2021-12-09 17:25:49 +0100

    Add solution for day3 part2

--- day3/day3.go
+++ day3/day3.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"strconv"
 
 	"github.com/joakimhew/aoc2021/utils"
 )
@@ -12,16 +13,23 @@ func main() {
 		log.Fatal(err)
 	}
 
-	incrementsPart1, err := calcPart1(inputLines, 12)
+	incrementsPart1, err := part1(inputLines, 12)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	log.Printf("part1 (gamma rate * epsilon rate): %d", incrementsPart1)
+
+	part2, err := part2(inputLines, 12)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("part2 (life support rating): %d", part2)
 }
 
 // calculateGammaAndEpsilonRatePart1 calculates the gamma and epsilon rate based on binary data with the most common gamma bit and the least common epsilon bit
-func calcPart1(input []string, width int) (int64, error) {
+func part1(input []string, width int) (int64, error) {
 	var gamma int64
 	var epsilon int64
 
@@ -46,4 +54,60 @@ func calcPart1(input []string, width int) (int64, error) {
 	}
 
 	return gamma * epsilon, nil
+}
+
+// part2 calculates the life support rating by multiplying the oxygen generator rating by the co2 scrubber rating.
+func part2(input []string, width int) (int, error) {
+	return search(input, oxygenFilter) * search(input, co2filter), nil
+}
+
+func search(input []string, filter func(rune, rune) rune) int {
+	remaining := make([]string, len(input))
+	copy(remaining, input)
+
+	offset := 0
+
+	for len(remaining) > 1 {
+		left := make([]string, 0, len(remaining))
+		target := filter(mostLeast(remaining, offset))
+
+		for _, line := range remaining {
+			if rune(line[offset]) == target {
+				left = append(left, line)
+			}
+		}
+
+		remaining = left
+		offset++
+	}
+
+	v, _ := strconv.ParseInt(remaining[0], 2, 32)
+	return int(v)
+}
+
+func oxygenFilter(most, _ rune) rune {
+	return most
+}
+
+func co2filter(_, least rune) rune {
+	return least
+}
+
+func mostLeast(input []string, idx int) (rune, rune) {
+	zeroes := 0
+	ones := 0
+
+	for _, line := range input {
+		if line[idx] == '0' {
+			zeroes++
+		} else {
+			ones++
+		}
+	}
+
+	if zeroes > ones {
+		return '0', '1'
+	}
+
+	return '1', '0'
 }
--- day3/day3_test.go
+++ day3/day3_test.go
@@ -2,8 +2,7 @@ package main
 
 import "testing"
 
-// testCalculateGammaAndEpsRatePart1
-func TestCalculateGammaAndEpsRatePart1(t *testing.T) {
+func TestPart1(t *testing.T) {
 	testCases := []struct {
 		data     []string
 		expected int64
@@ -28,7 +27,42 @@ func TestCalculateGammaAndEpsRatePart1(t *testing.T) {
 	}
 
 	for _, tc := range testCases {
-		actual, err := calcPart1(tc.data, 5)
+		actual, err := part1(tc.data, 5)
+		if err != nil {
+			t.Errorf("Error: %v", err)
+		}
+		if actual != tc.expected {
+			t.Errorf("Expected: %v, Actual: %v", tc.expected, actual)
+		}
+	}
+}
+
+func TestPart2(t *testing.T) {
+	testCases := []struct {
+		data     []string
+		expected int
+	}{
+		{
+			data: []string{
+				"00100",
+				"11110",
+				"10110",
+				"10111",
+				"10101",
+				"01111",
+				"00111",
+				"11100",
+				"10000",
+				"11001",
+				"00010",
+				"01010",
+			},
+			expected: 230,
+		},
+	}
+
+	for _, tc := range testCases {
+		actual, err := part2(tc.data, 5)
 		if err != nil {
 			t.Errorf("Error: %v", err)
 		}
